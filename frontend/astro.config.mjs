2094b4e5da1c429a3eb006278436e7b7b0f1d20b
Viktor Nasonov <qxuken@gmail.com>
2023-07-05 23:55:21 +0300

    Added frontend configurations(tailwind, prettier)

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import prefetch from '@astrojs/prefetch';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [prefetch(), tailwind()],
+});
