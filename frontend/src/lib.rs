9e4c111358da3cbc2b515b758a71da5d23ed70ed
Viktor Nasonov <qxuken@gmail.com>
2023-07-05 16:32:12 +0300

    Added 404 page handler

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,8 +1,18 @@
-use actix_files::Files;
+use actix_files::{Files, NamedFile};
+use actix_web::dev::{fn_service, ServiceRequest, ServiceResponse};
 
 pub fn create_service<'a>(url_path: &'static str, dir_path: Option<String>) -> Files {
     let dir = dir_path.unwrap_or("./frontend/dist/".to_string());
-    Files::new(url_path, dir)
+    Files::new(url_path, &dir.to_owned())
         .prefer_utf8(true)
         .index_file("index.html")
+        .default_handler(fn_service(move |req: ServiceRequest| {
+            let not_found_page_path = dir.clone() + "404.html";
+            async move {
+                let (req, _) = req.into_parts();
+                let file = NamedFile::open_async(not_found_page_path).await?;
+                let res = file.into_response(&req);
+                Ok(ServiceResponse::new(req, res))
+            }
+        }))
 }
